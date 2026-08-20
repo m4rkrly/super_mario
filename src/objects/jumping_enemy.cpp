b67dcde9837a9a953ec880c58dce59618b39840b
m4rkrly <w1ltsh1rem4rk@gmail.com>
2026-08-20 12:39:01 +0300

    JumpingEnemy: Initial creation

---
+++ src/objects/jumping_enemy.cpp
@@ -0,0 +1,45 @@
+#include "jumping_enemy.hpp"
+
+using biv::JumpingEnemy;
+
+JumpingEnemy::JumpingEnemy(
+  const Coord& top_left, const int width, const int height
+) : RectMapMovableAdapter(top_left, width, height) {
+  vspeed = 0;
+  hspeed = 0;
+}
+
+biv::Rect JumpingEnemy::get_rect() const noexcept {
+	return {top_left, width, height};
+}
+
+biv::Speed JumpingEnemy::get_speed() const noexcept {
+	return {vspeed, hspeed};
+}
+
+void JumpingEnemy::move_horizontally() noexcept {}
+
+void JumpingEnemy::move_vertically() noexcept {
+ 	jump();
+ 	if (vspeed < MAX_V_SPEED) {
+		vspeed += V_ACCELERATION;
+	}
+	top_left.y += vspeed;
+}
+
+void JumpingEnemy::process_horizontal_static_collision(Rect* obj) noexcept {}
+
+void JumpingEnemy::process_mario_collision(Collisionable* mario) noexcept {
+	if (mario->get_speed().v > 0 && mario->get_speed().v != V_ACCELERATION) {
+		kill();
+	} else {
+		mario->kill();
+	}
+}
+
+void JumpingEnemy::process_vertical_static_collision(Rect* obj) noexcept {
+	if (vspeed > 0) {
+		top_left.y -= vspeed;
+		vspeed = 0;
+	}
+}
